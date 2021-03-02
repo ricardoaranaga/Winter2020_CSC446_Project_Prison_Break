c6a2d056bbd2a22bc9daa573fc30d36b7ce1c880
Ricardo Aranaga <raa010@email.latech.edu>
2021-03-02 13:48:39 -0600

    added the hidden file creation to install

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ fi
 sudo ./source_code/Prison_Builder/make_prison_FTP.sh
 sudo ./source_code/Prison_Builder/make_prison_SSH.sh
 
-sudo python3 ./source_code/Prison_Builder/Layer_2.py 
+sudo python3 ./source_code/Layer\ -\ 2\ Prison/Layer_2.py
+cd source_code/Layer\ -\ 3\ Stegging/
+sudo python3 store_normal_rev_Steg.py -b
 
 sudo chown -R billythekid:billythekid /var/prison/home/billythekid
 sudo chown -R patgarret:patgarret /var/ftp/patgarret/
